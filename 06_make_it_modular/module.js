7563f06a137bbe22e1425ad20f83ffeaacf3d751
Tachi <zaveruha007@gmail.com>
2015-06-30 17:03:34 +0300

    Module

--- 06_make_it_modular/module.js
+++ 06_make_it_modular/module.js
@@ -6,19 +6,16 @@ module.exports = myModule;
 function myModule(dirname, filter, callback) {
   var ext = '.' + filter;
   var filelist = [];
-  var i = 0;
 
   fs.readdir(dirname, function(err, list) {
     if (err) {
       return callback(err);
     } else {
-      var ln = list.length;
-      var i = 0;
-      for (i; i < ln; i++) {
-        if (path.extname(list[i]) == ext) {
-          filelist.push(list[i]);
+      list.forEach(function(file){
+        if (path.extname(file) == ext) {
+          filelist.push(file);
         }
-      }
+      });
     }
     return callback(null, filelist);
   });
--- 06_make_it_modular/program.js
+++ 06_make_it_modular/program.js
@@ -7,9 +7,8 @@ mymodule(dir, ext, function(err, list) {
   if (err)
     throw err;
   else {
-    var ln = list.length;
-    var i = 0;
-    for (i; i < ln; i++)
-      console.log(list[i]);
+    list.forEach(function(file){
+      console.log(file);
+    });
   }
 });
