e80ba4e030cf03cca080ddc89a234e6296cc6478
Tachi <zaveruha007@gmail.com>
2015-06-30 14:35:14 +0300

    Async I/O

---
+++ 05_filtered_ls/program.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+//Directory Path
+var dir = process.argv[2];
+//File Extension
+var ext = process.argv[3];
+
+fs.readdir(dir, function(err, list) {
+  var ln = list.length;
+  var i = 0;
+  for (i; i < ln; i++) {
+    if (path.extname(list[i]) == '.'+ext) {
+      console.log(list[i]);
+    }
+  }
+});
