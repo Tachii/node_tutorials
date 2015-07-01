e25800debc7417fa274e085b31e16d17d982ec77
Tachi <zaveruha007@gmail.com>
2015-07-01 18:57:00 +0300

    Time Server

--- 10_time_server/program_01.js
+++ 10_time_server/program_01.js
@@ -1,13 +1,18 @@
 var net = require('net');
-var date = new Date();
 
-var server = net.createServer(function(socket) {
+function timeString() {
+  var date = new Date();
   var timestring =
     date.getFullYear() + "-" +
     ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
     ("0" + (date.getDate())).slice(-2) + " " +
     date.getHours() + ":" +
     date.getMinutes() + "\n";
-  socket.end(timestring);
+  return timestring;
+}
+
+var server = net.createServer(function(socket) {
+  socket.end(timeString());
 });
-server.listen(process.argv[2]);
+
+server.listen(Number(process.argv[2]));
