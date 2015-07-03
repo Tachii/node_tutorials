57b2bbe2ca10b7431cc4d7a5270736342a8c4478
Tachi <zaveruha007@gmail.com>
2015-07-03 15:08:18 +0300

    http json api server

---
+++ 13_http_json_api_server/program.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var url = require('url');
+
+function parseRoutes(url) {
+  if (url.pathname === '/api/parsetime') {
+    var date = new Date(url.query.iso);
+    return {
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    };
+  } else if (url.pathname === '/api/unixtime') {
+    return {
+      unixtime: new Date(url.query.iso).getTime()
+    };
+  } else return null;
+}
+
+var server = http.createServer(function(req, res) {
+  var urlPath = url.parse(req.url, true);
+  var resourse = parseRoutes(urlPath);
+  
+  if (resourse) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify(resourse));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+
+  res.end(JSON.stringify(resourse));
+
+});
+server.listen(process.argv[2]);
